c8e8efaa63469d8b8bde8ca3e92cab2aa9fb897a
Johan Hillerström <progr@mmer.nu>
2017-12-04 21:14:47 +0100

    Use less derpy way of counting uniques

--- Day04/Main.elm
+++ Day04/Main.elm
@@ -2,8 +2,7 @@ module Day04.Main exposing (main)
 
 import Html exposing (..)
 import Day04.Input exposing (parsedInput)
-import Helpers.Helpers exposing (trigger, Delay(..))
-import Dict exposing (..)
+import Helpers.Helpers exposing (trigger, Delay(..), count, unique)
 
 
 type alias Model =
@@ -15,11 +14,6 @@ type alias Model =
 
 type Msg
     = Run
-
-
-type Part
-    = PartOne
-    | PartTwo
 
 
 main : Program Never Model Msg
@@ -41,93 +35,14 @@ init =
         ! [ trigger NoDelay Run ]
 
 
-isOnlyOne : List a -> a -> Bool
-isOnlyOne xs x =
-    (List.length <| List.filter ((==) x) xs) == 1
+isUnique : List comparable -> Bool
+isUnique w =
+    List.sort w == unique w
 
 
-updateDictValue : List a -> a -> Maybe Int -> Maybe Int
-updateDictValue chars key =
-    let
-        cnt =
-            List.length <| List.filter ((==) key) chars
-    in
-        Just << Maybe.withDefault cnt << Maybe.map ((+) cnt)
-
-
-getUnique : List comparable -> Dict comparable Int
-getUnique chars =
-    List.foldl (\a -> Dict.update a (updateDictValue chars a)) Dict.empty chars
-
-
-getChars : String -> ( Int, Dict Char Int )
-getChars word =
-    let
-        chars =
-            String.toList word
-
-        unique =
-            getUnique chars
-    in
-        ( List.length chars, unique )
-
-
-alwaysFail : a -> c -> b -> number
-alwaysFail k a =
-    always 1
-
-
-valuesEqual : a -> b -> b -> number -> number
-valuesEqual k a b x =
-    if a == b then
-        x
-    else
-        x + 1
-
-
-compareUnique : Dict Char Int -> Dict Char Int -> Bool
-compareUnique first second =
-    (Dict.merge alwaysFail valuesEqual alwaysFail first second 0) == 0
-
-
-isAnagram : Int -> Dict Char Int -> String -> Bool
-isAnagram charCount unique word =
-    let
-        ( c, u ) =
-            getChars word
-    in
-        c == charCount && compareUnique unique u
-
-
-noAnagram : List String -> String -> Bool
-noAnagram words word =
-    let
-        ( charCount, unique ) =
-            getChars word
-
-        anagrams =
-            words
-                |> List.filter (isAnagram charCount unique)
-                |> List.length
-    in
-        anagrams == 1
-
-
-isValid : Part -> String -> Bool
-isValid part str =
-    let
-        words =
-            String.words str
-
-        func =
-            case part of
-                PartOne ->
-                    isOnlyOne words
-
-                PartTwo ->
-                    (\w -> isOnlyOne words w && noAnagram words w)
-    in
-        List.all func words
+isUniqueWithAnagrams : List String -> Bool
+isUniqueWithAnagrams =
+    isUnique << List.map (String.fromList << List.sort << String.toList)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -135,15 +50,15 @@ update msg model =
     case msg of
         Run ->
             let
-                firstPartCount =
+                passwords =
                     model.input
-                        |> List.filter (isValid PartOne)
-                        |> List.length
+                        |> List.map String.words
+
+                firstPartCount =
+                    count isUnique passwords
 
                 secondPartCount =
-                    model.input
-                        |> List.filter (isValid PartTwo)
-                        |> List.length
+                    count isUniqueWithAnagrams passwords
             in
                 { model
                     | firstPartCount = firstPartCount
