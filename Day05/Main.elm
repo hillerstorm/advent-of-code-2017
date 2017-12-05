b9118db270a4a9413e3356271ca0917ddae8a446
Johan Hillerström <progr@mmer.nu>
2017-12-05 10:30:09 +0100

    Add day 5 part 2

--- Day05/Main.elm
+++ Day05/Main.elm
@@ -6,11 +6,21 @@ import Day05.Input exposing (parsedInput)
 import Helpers.Helpers exposing (trigger, Delay(..))
 
 
+type State
+    = Running
+    | Done
+
+
 type alias Model =
     { input : List Int
-    , current : List Int
-    , idx : Int
-    , steps : Int
+    , firstList : List Int
+    , firstIndex : Int
+    , firstSteps : Int
+    , firstState : State
+    , secondList : List Int
+    , secondIndex : Int
+    , secondSteps : Int
+    , secondState : State
     }
 
 
@@ -31,37 +41,90 @@ main =
 init : ( Model, Cmd Msg )
 init =
     { input = parsedInput
-    , current = parsedInput
-    , idx = 0
-    , steps = 0
+    , firstList = parsedInput
+    , firstIndex = 0
+    , firstSteps = 0
+    , firstState = Running
+    , secondList = parsedInput
+    , secondIndex = 0
+    , secondSteps = 0
+    , secondState = Running
     }
         ! [ trigger NoDelay Jump ]
+
+
+type Part
+    = First
+    | Second
+
+
+getNextValues : List Int -> Int -> Int -> State -> Part -> ( List Int, Int, Int, State )
+getNextValues list index steps state part =
+    case state of
+        Running ->
+            case getAt index list of
+                Just x ->
+                    let
+                        offsetFunc =
+                            case part of
+                                First ->
+                                    (+)
+
+                                Second ->
+                                    if x >= 3 then
+                                        (-)
+                                    else
+                                        (+)
+                    in
+                        case setAt index (offsetFunc x 1) list of
+                            Just y ->
+                                ( y, index + x, steps + 1, Running )
+
+                            Nothing ->
+                                ( list, index + x, steps + 1, Running )
+
+                Nothing ->
+                    ( list, index, steps, Done )
+
+        Done ->
+            ( list, index, steps, state )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Jump ->
-            case getAt model.idx model.current of
-                Just x ->
-                    case setAt model.idx (x + 1) model.current of
-                        Just y ->
-                            { model
-                                | current = y
-                                , idx = model.idx + x
-                                , steps = model.steps + 1
-                            }
-                                ! [ trigger NoDelay Jump ]
+            let
+                ( nextFirstList, nextFirstIndex, nextFirstSteps, nextFirstState ) =
+                    getNextValues model.firstList model.firstIndex model.firstSteps model.firstState First
 
-                        Nothing ->
-                            { model | steps = model.steps + 1 } ! []
+                ( nextSecondList, nextSecondIndex, nextSecondSteps, nextSecondState ) =
+                    getNextValues model.secondList model.secondIndex model.secondSteps model.secondState Second
 
-                Nothing ->
-                    model ! []
+                nextCmd =
+                    case ( nextFirstState, nextSecondState ) of
+                        ( Done, Done ) ->
+                            []
+
+                        _ ->
+                            [ trigger NoDelay Jump ]
+            in
+                { model
+                    | firstList = nextFirstList
+                    , firstIndex = nextFirstIndex
+                    , firstSteps = nextFirstSteps
+                    , firstState = nextFirstState
+                    , secondList = nextSecondList
+                    , secondIndex = nextSecondIndex
+                    , secondSteps = nextSecondSteps
+                    , secondState = nextSecondState
+                }
+                    ! nextCmd
 
 
 view : Model -> Html msg
 view model =
     div []
-        [ div [] [ text <| "Part 1: " ++ toString model.steps ]
+        [ div [] [ text <| "Part 1: " ++ toString model.firstSteps ]
+        , div [] [ text <| "Part 2: " ++ toString model.secondSteps ]
         ]
