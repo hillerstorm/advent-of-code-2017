2475553d33386ec09573fd2301690e9e623b3f91
Johan Hillerström <progr@mmer.nu>
2017-12-13 23:20:34 +0100

    Add day 13

---
+++ Day13/Main.elm
@@ -0,0 +1,144 @@
+module Day13.Main exposing (main)
+
+import Html exposing (..)
+import Day13.Input exposing (rawInput)
+import Helpers.Helpers exposing (trigger, Delay(..), prettyMaybe)
+
+
+type Input
+    = NotParsed
+    | Parsed (List ( Int, Int ))
+
+
+type alias Model =
+    { input : String
+    , parsedInput : Input
+    , firstPart : Maybe Int
+    , secondPart : Maybe Int
+    }
+
+
+type Msg
+    = Parse
+    | Run
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        , view = view
+        }
+
+
+init : ( Model, Cmd Msg )
+init =
+    { input = rawInput
+    , parsedInput = NotParsed
+    , firstPart = Nothing
+    , secondPart = Nothing
+    }
+        ! [ trigger NoDelay Parse ]
+
+
+toTuple : String -> ( Int, Int )
+toTuple str =
+    case String.split ": " str of
+        [ k, v ] ->
+            case ( String.toInt k, String.toInt v ) of
+                ( Ok key, Ok value ) ->
+                    ( key, value )
+
+                _ ->
+                    Debug.crash "Invalid pair"
+
+        _ ->
+            Debug.crash "Invalid line"
+
+
+parse : String -> Input
+parse =
+    Parsed << List.map toTuple << String.lines
+
+
+getIdx : Int -> Int -> Int
+getIdx key value =
+    let
+        len =
+            (value * 2) - 2
+
+        idx =
+            key % len
+    in
+        if idx < value then
+            idx
+        else
+            len - idx
+
+
+getSeverity : ( Int, Int ) -> Int
+getSeverity ( key, value ) =
+    if getIdx key value == 0 then
+        key * value
+    else
+        0
+
+
+isElsewhere : Int -> ( Int, Int ) -> Bool
+isElsewhere offset ( key, value ) =
+    getIdx (key + offset) value > 0
+
+
+findOffset : List ( Int, Int ) -> Int -> Int
+findOffset layers offset =
+    if List.all (isElsewhere offset) layers then
+        offset
+    else
+        findOffset layers <| offset + 1
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Parse ->
+            { model
+                | parsedInput = parse model.input
+            }
+                ! [ trigger NoDelay Run ]
+
+        Run ->
+            case model.parsedInput of
+                NotParsed ->
+                    model ! [ trigger NoDelay Parse ]
+
+                Parsed layers ->
+                    let
+                        severity =
+                            layers
+                                |> List.map getSeverity
+                                |> List.sum
+
+                        minOffset =
+                            findOffset layers 0
+                    in
+                        { model
+                            | firstPart = Just severity
+                            , secondPart = Just minOffset
+                        }
+                            ! []
+
+
+view : Model -> Html msg
+view model =
+    div []
+        (case model.parsedInput of
+            NotParsed ->
+                [ div [] [ text "Parsing..." ] ]
+
+            Parsed input ->
+                [ div [] [ text <| "Part 1: " ++ prettyMaybe model.firstPart ]
+                , div [] [ text <| "Part 2: " ++ prettyMaybe model.secondPart ]
+                ]
+        )
